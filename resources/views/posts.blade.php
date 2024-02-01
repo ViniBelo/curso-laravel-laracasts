25be5acc611ee4dccf6c79c296b36c980fb1add0
Vinicius Alves Beló <alexguilhermeferreiraa@gmail.com>
2024-02-01 10:14:38 -0300

    Make a route and link to it

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="app.css">
+    <title>My Blog</title>
+</head>
+<body>
+    <article>
+        <h1><a href="/post">My First Post</a></h1>
+        <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sint, nesciunt! Reiciendis voluptas libero ullam facere odit ipsa mollitia, placeat ducimus omnis voluptates optio deserunt, obcaecati officiis alias saepe laboriosam dignissimos. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Debitis odit, perferendis, vitae alias veniam harum similique illo voluptate, cupiditate modi nesciunt aliquam laboriosam nemo. Quae magnam quasi aut pariatur ea! Lorem ipsum dolor sit amet consectetur adipisicing elit. Error nisi assumenda vitae rem fugit reiciendis dolor debitis! Ut commodi ducimus at voluptatibus dolorum voluptatem odit, natus optio, sit saepe magnam!
+        </p>
+    </article>
+    <article>
+        <h1><a href="/post">My Second Post</a></h1>
+        <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sint, nesciunt! Reiciendis voluptas libero ullam facere odit ipsa mollitia, placeat ducimus omnis voluptates optio deserunt, obcaecati officiis alias saepe laboriosam dignissimos. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Debitis odit, perferendis, vitae alias veniam harum similique illo voluptate, cupiditate modi nesciunt aliquam laboriosam nemo. Quae magnam quasi aut pariatur ea! Lorem ipsum dolor sit amet consectetur adipisicing elit. Error nisi assumenda vitae rem fugit reiciendis dolor debitis! Ut commodi ducimus at voluptatibus dolorum voluptatem odit, natus optio, sit saepe magnam!
+        </p>
+    </article>
+    <article>
+        <h1><a href="/post">My Third Post</a></h1>
+        <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sint, nesciunt! Reiciendis voluptas libero ullam facere odit ipsa mollitia, placeat ducimus omnis voluptates optio deserunt, obcaecati officiis alias saepe laboriosam dignissimos. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Debitis odit, perferendis, vitae alias veniam harum similique illo voluptate, cupiditate modi nesciunt aliquam laboriosam nemo. Quae magnam quasi aut pariatur ea! Lorem ipsum dolor sit amet consectetur adipisicing elit. Error nisi assumenda vitae rem fugit reiciendis dolor debitis! Ut commodi ducimus at voluptatibus dolorum voluptatem odit, natus optio, sit saepe magnam!
+        </p>
+    </article>
+</body>
+</html>
