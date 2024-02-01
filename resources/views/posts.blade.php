bcc76252b8ed3f9364d025f77e4afe6f3ad41717
Vinicius Alves Beló <alexguilhermeferreiraa@gmail.com>
2024-02-01 11:51:15 -0300

    Use the Filesystem Class to Read a Directory

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -7,23 +7,10 @@
     <title>My Blog</title>
 </head>
 <body>
+    <?php foreach ($posts as $post) : ?>
     <article>
-        <h1><a href="/posts/my-first-post">My First Post</a></h1>
-        <p>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sint, nesciunt! Reiciendis voluptas libero ullam facere odit ipsa mollitia, placeat ducimus omnis voluptates optio deserunt, obcaecati officiis alias saepe laboriosam dignissimos. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Debitis odit, perferendis, vitae alias veniam harum similique illo voluptate, cupiditate modi nesciunt aliquam laboriosam nemo. Quae magnam quasi aut pariatur ea! Lorem ipsum dolor sit amet consectetur adipisicing elit. Error nisi assumenda vitae rem fugit reiciendis dolor debitis! Ut commodi ducimus at voluptatibus dolorum voluptatem odit, natus optio, sit saepe magnam!
-        </p>
+        <?= $post; ?>
     </article>
-    <article>
-        <h1><a href="/posts/my-second-post">My Second Post</a></h1>
-        <p>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sint, nesciunt! Reiciendis voluptas libero ullam facere odit ipsa mollitia, placeat ducimus omnis voluptates optio deserunt, obcaecati officiis alias saepe laboriosam dignissimos. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Debitis odit, perferendis, vitae alias veniam harum similique illo voluptate, cupiditate modi nesciunt aliquam laboriosam nemo. Quae magnam quasi aut pariatur ea! Lorem ipsum dolor sit amet consectetur adipisicing elit. Error nisi assumenda vitae rem fugit reiciendis dolor debitis! Ut commodi ducimus at voluptatibus dolorum voluptatem odit, natus optio, sit saepe magnam!
-        </p>
-    </article>
-    <article>
-        <h1><a href="/posts/my-third-post">My Third Post</a></h1>
-        <p>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sint, nesciunt! Reiciendis voluptas libero ullam facere odit ipsa mollitia, placeat ducimus omnis voluptates optio deserunt, obcaecati officiis alias saepe laboriosam dignissimos. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Debitis odit, perferendis, vitae alias veniam harum similique illo voluptate, cupiditate modi nesciunt aliquam laboriosam nemo. Quae magnam quasi aut pariatur ea! Lorem ipsum dolor sit amet consectetur adipisicing elit. Error nisi assumenda vitae rem fugit reiciendis dolor debitis! Ut commodi ducimus at voluptatibus dolorum voluptatem odit, natus optio, sit saepe magnam!
-        </p>
-    </article>
+    <?php endforeach; ?>
 </body>
 </html>
