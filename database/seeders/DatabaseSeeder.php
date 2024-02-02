cf8d15b30d18d2c878373f2d0c5cd55b5b4bc93f
Vinicius Alves Beló <alexguilhermeferreiraa@gmail.com>
2024-02-02 15:02:02 -0300

    Turbo Boost With Factories

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,52 +15,55 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        User::truncate();
-        Post::truncate();
-        Category::truncate();
-
-        $user = User::factory()->create();
-
-        $personal = Category::create([
-            'name' => 'Personal',
-            'slug' => 'personal'
+    $user = User::factory()->create([
+            'name'=>'John Doe'
+        ]);
+        Post::factory(5)->create([
+            'user_id' => $user->id
         ]);
 
-        $family = Category::create([
-            'name' => 'Family',
-            'slug' => 'Family'
-        ]);
+        // $user = User::factory()->create();
 
-        $work = Category::create([
-            'name' => 'Work',
-            'slug' => 'work'
-        ]);
+        // $personal = Category::create([
+        //     'name' => 'Personal',
+        //     'slug' => 'personal'
+        // ]);
 
-        Post::create([
-            'user_id'=>$user->id,
-            'category_id'=>$family->id,
-            'title'=>'My Family Post',
-            'slug'=>'my-first-post',
-            'excerpt'=>'<p>Lorem ipsum dolar sit amet.</p>',
-            'body'=>'<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus commodi ab omnis, optio fugit voluptates! Sunt neque doloribus provident nulla autem nihil ipsam iure animi magni sequi accusamus, vitae et.Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus commodi ab omnis, optio fugit voluptates! Sunt neque doloribus provident nulla autem nihil ipsam iure animi magni sequi accusamus, vitae et.Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus commodi ab omnis, optio fugit voluptates! Sunt neque doloribus provident nulla autem nihil ipsam iure animi magni sequi accusamus, vitae et.Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus commodi ab omnis, optio fugit voluptates! Sunt neque doloribus provident nulla autem nihil ipsam iure animi magni sequi accusamus, vitae et.Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus commodi ab omnis, optio fugit voluptates! Sunt neque doloribus provident nulla autem nihil ipsam iure animi magni sequi accusamus, vitae et.</p>'
-        ]);
+        // $family = Category::create([
+        //     'name' => 'Family',
+        //     'slug' => 'Family'
+        // ]);
 
-        Post::create([
-            'user_id'=>$user->id,
-            'category_id'=>$work->id,
-            'title'=>'My Work Post',
-            'slug'=>'my-work-post',
-            'excerpt'=>'<p>Lorem ipsum dolar sit amet.</p>',
-            'body'=>'<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus commodi ab omnis, optio fugit voluptates! Sunt neque doloribus provident nulla autem nihil ipsam iure animi magni sequi accusamus, vitae et.Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus commodi ab omnis, optio fugit voluptates! Sunt neque doloribus provident nulla autem nihil ipsam iure animi magni sequi accusamus, vitae et.Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus commodi ab omnis, optio fugit voluptates! Sunt neque doloribus provident nulla autem nihil ipsam iure animi magni sequi accusamus, vitae et.Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus commodi ab omnis, optio fugit voluptates! Sunt neque doloribus provident nulla autem nihil ipsam iure animi magni sequi accusamus, vitae et.Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus commodi ab omnis, optio fugit voluptates! Sunt neque doloribus provident nulla autem nihil ipsam iure animi magni sequi accusamus, vitae et.</p>'
-        ]);
+        // $work = Category::create([
+        //     'name' => 'Work',
+        //     'slug' => 'work'
+        // ]);
 
-        Post::create([
-            'user_id'=>$user->id,
-            'category_id'=>$personal->id,
-            'title'=>'My Personal Post',
-            'slug'=>'my-personal-post',
-            'excerpt'=>'<p>Lorem ipsum dolar sit amet.</p>',
-            'body'=>'<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus commodi ab omnis, optio fugit voluptates! Sunt neque doloribus provident nulla autem nihil ipsam iure animi magni sequi accusamus, vitae et.Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus commodi ab omnis, optio fugit voluptates! Sunt neque doloribus provident nulla autem nihil ipsam iure animi magni sequi accusamus, vitae et.Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus commodi ab omnis, optio fugit voluptates! Sunt neque doloribus provident nulla autem nihil ipsam iure animi magni sequi accusamus, vitae et.Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus commodi ab omnis, optio fugit voluptates! Sunt neque doloribus provident nulla autem nihil ipsam iure animi magni sequi accusamus, vitae et.Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus commodi ab omnis, optio fugit voluptates! Sunt neque doloribus provident nulla autem nihil ipsam iure animi magni sequi accusamus, vitae et.</p>'
-        ]);
+        // Post::create([
+        //     'user_id'=>$user->id,
+        //     'category_id'=>$family->id,
+        //     'title'=>'My Family Post',
+        //     'slug'=>'my-first-post',
+        //     'excerpt'=>'<p>Lorem ipsum dolar sit amet.</p>',
+        //     'body'=>'<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus commodi ab omnis, optio fugit voluptates! Sunt neque doloribus provident nulla autem nihil ipsam iure animi magni sequi accusamus, vitae et.Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus commodi ab omnis, optio fugit voluptates! Sunt neque doloribus provident nulla autem nihil ipsam iure animi magni sequi accusamus, vitae et.Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus commodi ab omnis, optio fugit voluptates! Sunt neque doloribus provident nulla autem nihil ipsam iure animi magni sequi accusamus, vitae et.Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus commodi ab omnis, optio fugit voluptates! Sunt neque doloribus provident nulla autem nihil ipsam iure animi magni sequi accusamus, vitae et.Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus commodi ab omnis, optio fugit voluptates! Sunt neque doloribus provident nulla autem nihil ipsam iure animi magni sequi accusamus, vitae et.</p>'
+        // ]);
+
+        // Post::create([
+        //     'user_id'=>$user->id,
+        //     'category_id'=>$work->id,
+        //     'title'=>'My Work Post',
+        //     'slug'=>'my-work-post',
+        //     'excerpt'=>'<p>Lorem ipsum dolar sit amet.</p>',
+        //     'body'=>'<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus commodi ab omnis, optio fugit voluptates! Sunt neque doloribus provident nulla autem nihil ipsam iure animi magni sequi accusamus, vitae et.Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus commodi ab omnis, optio fugit voluptates! Sunt neque doloribus provident nulla autem nihil ipsam iure animi magni sequi accusamus, vitae et.Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus commodi ab omnis, optio fugit voluptates! Sunt neque doloribus provident nulla autem nihil ipsam iure animi magni sequi accusamus, vitae et.Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus commodi ab omnis, optio fugit voluptates! Sunt neque doloribus provident nulla autem nihil ipsam iure animi magni sequi accusamus, vitae et.Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus commodi ab omnis, optio fugit voluptates! Sunt neque doloribus provident nulla autem nihil ipsam iure animi magni sequi accusamus, vitae et.</p>'
+        // ]);
+
+        // Post::create([
+        //     'user_id'=>$user->id,
+        //     'category_id'=>$personal->id,
+        //     'title'=>'My Personal Post',
+        //     'slug'=>'my-personal-post',
+        //     'excerpt'=>'<p>Lorem ipsum dolar sit amet.</p>',
+        //     'body'=>'<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus commodi ab omnis, optio fugit voluptates! Sunt neque doloribus provident nulla autem nihil ipsam iure animi magni sequi accusamus, vitae et.Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus commodi ab omnis, optio fugit voluptates! Sunt neque doloribus provident nulla autem nihil ipsam iure animi magni sequi accusamus, vitae et.Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus commodi ab omnis, optio fugit voluptates! Sunt neque doloribus provident nulla autem nihil ipsam iure animi magni sequi accusamus, vitae et.Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus commodi ab omnis, optio fugit voluptates! Sunt neque doloribus provident nulla autem nihil ipsam iure animi magni sequi accusamus, vitae et.Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus commodi ab omnis, optio fugit voluptates! Sunt neque doloribus provident nulla autem nihil ipsam iure animi magni sequi accusamus, vitae et.</p>'
+        // ]);
     }
 }
