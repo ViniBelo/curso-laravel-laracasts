0ecf5f712d83c977afb67ff7ceac723cb5b6102b
Vinicius Alves Beló <alexguilhermeferreiraa@gmail.com>
2024-02-02 15:29:28 -0300

    View All Posts By An Author

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,12 +15,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-    $user = User::factory()->create([
-            'name'=>'John Doe'
-        ]);
-        Post::factory(5)->create([
-            'user_id' => $user->id
-        ]);
+        Post::factory(5)->create();
 
         // $user = User::factory()->create();
 
