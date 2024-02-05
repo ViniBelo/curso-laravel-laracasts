4b39af7d231b705d05112174e1dbd977d899d263
ViniBelo <vinialvesbelo@outlook.com>
2024-02-04 21:24:57 -0300

    Convert the HTML and CSS to Blade

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,5 +1,26 @@
 <x-layout>
-    @foreach ($posts as $post)
+    @include('_post-header')
+
+    <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
+
+        <x-post-featured />
+
+        <div class="lg:grid lg:grid-cols-2">
+
+            <x-post-card />
+            <x-post-card />
+
+        </div>
+
+        <div class="lg:grid lg:grid-cols-3">
+
+            <x-post-card />
+            <x-post-card />
+            <x-post-card />
+
+        </div>
+    </main>
+    {{-- @foreach ($posts as $post)
         <article class="{{ $loop->even ? 'mb-6' : '' }}">
             <h1>
                 <a href="/posts/{{ $post->slug }}">
@@ -17,5 +38,5 @@
                 {!! $post->excerpt !!}
             </div>
         </article>
-    @endforeach
+    @endforeach --}}
 </x-layout>
