bf51e8d3371149c0ad19a85159b6241eba8c037c
Vinicius Alves Beló <alexguilhermeferreiraa@gmail.com>
2024-02-06 14:58:33 -0300

    Write the Markup for a Post Comment

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -23,7 +23,7 @@ class PostController extends Controller
 
     public function show(Post $post)
     {
-        return view('post.show', [
+        return view('posts.show', [
             'post' => $post
         ]);
     }
