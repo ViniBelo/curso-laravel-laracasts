65b8f367b21f440582f89bbb7d56619ad26ea3e2
Vinicius Alves Beló <alexguilhermeferreiraa@gmail.com>
2024-02-06 14:24:33 -0300

    Build the Log In Page

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -6,9 +6,34 @@ use Illuminate\Http\Request;
 
 class SessionsController extends Controller
 {
+    public function create()
+    {
+        return view('sessions.create');
+    }
+
+    public function store()
+    {
+        $attributes = request()->validate
+        ([
+            'email' => 'required|email',
+            'password' => 'required'
+        ]);
+
+        if(auth()->attempt($attributes))
+        {
+            return redirect('/')->with('success', 'Welcome Back!');
+        }
+
+        return back()->withErrors
+        ([
+            'email' => 'The provided credentials could not be verified.'
+        ])
+        ->withInput();
+    }
+
     public function destroy()
     {
-        auth()->logout();
+        session()->regenerate();
 
         return redirect('/')->with('success', 'Goodbye!');
     }
